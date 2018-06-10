c741ce57a68deeb4b363bd6fa30cf1b344ad00db
Edsel Caprice <caprice@mail.usf.edu>
2018-06-10 03:47:39 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,46 @@
+def turn
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def valid_move?(board,index)
+  
+  if  position_taken?(board,index)
+  
+    return nil
+    
+   elsif index.between?(0,8)
+   
+    return !position_taken?(board,index)
+    
+  end
+end
+
+def position_taken?(board,index)
+  
+  if board[index] == " " || board[index] == nil || board[index] == ""
+    return FALSE
+    
+  elsif board[index] == "X" || board[index] == "O"
+    return TRUE
+    
+  end
+end
+
+def input_to_index(input)
+  
+  return input.to_i - 1
+  
+end
+
+def move(array,index,value = "X")
+   array[index] = value
+   return array
+end
+
+end #ends turn method
